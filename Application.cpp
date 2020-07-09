551855caa7e391e47c2fa0ff3d9cc290d8503517
vamsi60 <vamsikarthik67@gmail.com>
2020-07-09 23:30:17 +0530

    Shader Abstraction

--- Application.cpp
+++ Application.cpp
@@ -4,53 +4,7 @@
 
 #include "VertexArray.h"
 #include "IndexBuffer.h"
-//#include "VertexBuffer.h"
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-    unsigned int id = glCreateShader(type);
-    const char * src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    // TO DO Error Handling
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if(result == GL_FALSE)
-    {   
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char *message = (char *)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to Compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!"; 
-        std::cout << std::endl;
-
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }   
-
-    return id; 
-    
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    
-    return program;
-}
-
+#include "Shader.h"
 
 int main()
 {
@@ -125,16 +79,15 @@ int main()
     	"   color = u_Color;\n"
     	"}\n";
 
-    	unsigned int shader = CreateShader(vertexShader, fragmentShader);
-    	glUseProgram(shader);
+		Shader shader("shaders/Basic.shader");
+		shader.Bind();
 
-    	int location = glGetUniformLocation(shader, "u_Color");
-    	glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
 		va.UnBind();
-    	glUseProgram(0);
-    	glBindBuffer(GL_ARRAY_BUFFER, 0);
-    	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
+    	shader.Unbind();
+		vb.UnBind();
+		ib.UnBind();
 
     	float r = 0.0f;
     	float increment = 0.05f;
@@ -145,10 +98,9 @@ int main()
         	/* Render here */
         	glClear(GL_COLOR_BUFFER_BIT);
 
-        	glUseProgram(shader);
-        	glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+        	shader.Bind();
+			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-        	//glBindVertexArray(vao);
 			va.Bind();
 			ib.Bind();
 
@@ -167,8 +119,6 @@ int main()
         	/* Poll for and process events */
         	glfwPollEvents();
 		}
-
-    	glDeleteProgram(shader);
 	}
 
     glfwTerminate();
--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 CXX = g++
 CXXFLAGS = -Wall -g -std=c++11
 
-MyApp : Application.o VertexArray.o VertexBuffer.o IndexBuffer.o
-	$(CXX) $(CXXFLAGS) -o MyApp Application.o VertexArray.o VertexBuffer.o IndexBuffer.o -L /home/vamsi/vamsi/OpenGL/practice/glew/build/lib -lGLEW -lglfw -lGL
+MyApp : Application.o VertexArray.o Shader.o VertexBuffer.o IndexBuffer.o
+	$(CXX) $(CXXFLAGS) -o MyApp Application.o VertexArray.o Shader.o VertexBuffer.o IndexBuffer.o -L /home/vamsi/vamsi/OpenGL/practice/glew/build/lib -lGLEW -lglfw -lGL
 
 Application.o : Application.cpp VertexArray.h IndexBuffer.h
 	$(CXX) $(CXXFLAGS) -c Application.cpp
 
-VertexArray.o : VertexArray.h VertexArray.cpp
+VertexArray.o : VertexArray.h
 
-VertexBuffer.o : VertexBuffer.h VertexBuffer.cpp
+VertexBuffer.o : VertexBuffer.h
 
-IndexBuffer.o : IndexBuffer.h IndexBuffer.cpp
+IndexBuffer.o : IndexBuffer.h
+
+Shader.o : Shader.h
 
